24bfe6d70733ba5934d2ccef3dcc58c192e17232
Yizack Rangel <yizackr@gmail.com>
2023-06-10 23:04:24 +0200

    feat: added `Twemojify` component

---
+++ src/runtime/components/Twemojify.vue
@@ -0,0 +1,56 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import { ref, watchEffect } from 'vue'
+import { parse } from '@twemoji/parser'
+import { useState } from '#imports'
+
+const props = defineProps({
+  text: {
+    type: String,
+    required: true
+  },
+  png: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const twemojify = useState('twemojify', () => ({}) as { [key: string]: string })
+const parsedText = ref(props.text)
+
+const emojis = parse(props.text, { assetType: props.png ? 'png' : 'svg' })
+
+for (const { url, indices, text } of emojis) {
+  if (twemojify.value[text]) {
+    parsedText.value = parsedText.value.replace(props.text.slice(...indices), twemojify.value[text])
+    continue
+  }
+  
+  if (props.png) {
+    twemojify.value[text] = `<img src="${url}" class="twemojify" />`
+  }
+  else {
+    const svgFetch = await $fetch(url).then(async (res: any) => await res.text()).catch(() => '')
+    const svgBody = svgFetch.replace(/<\/*svg[^>]*>/g, '')
+    const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36" class="twemojify">${svgBody}</svg>`
+    twemojify.value[text] = svg
+  }
+  parsedText.value = parsedText.value.replace(props.text.slice(...indices), twemojify.value[text])
+}
+
+watchEffect(() => parsedText.value)
+</script>
+
+<!-- eslint-disable vue/no-v-html -->
+<template>
+  <span v-html="parsedText" />
+</template>
+
+<style>
+.twemojify {
+  height: 1em;
+  width: 1em;
+  margin: 0 .05em 0 .1em;
+  vertical-align: -0.1em;
+}
+</style>
