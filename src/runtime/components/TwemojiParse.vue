274613b1c2131026c1cdc9663e3b2614fdc65838
Yizack Rangel <yizackr@gmail.com>
2023-06-10 23:04:40 +0200

    feat: added `TwemojiParse` component

---
+++ src/runtime/components/TwemojiParse.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import twemoji, { Twemoji } from '@twemoji/api'
+import { onMounted, ref } from 'vue'
+
+const props = defineProps({
+  png: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const twemojiParse = ref({} as HTMLElement)
+onMounted(() => {
+  (twemoji as Twemoji).parse(twemojiParse.value, {
+    ext: props.png ? '.png' : '.svg',
+    folder: props.png ? undefined : 'svg',
+    className: 'twemojiParse'
+  })
+})
+</script>
+
+<template>
+  <span ref="twemojiParse">
+    <slot />
+  </span>
+</template>
+
+<style>
+img.twemojiParse {
+  height: 1em;
+  width: 1em;
+  margin: 0 .05em 0 .1em;
+  vertical-align: -0.1em;
+}
+</style>
