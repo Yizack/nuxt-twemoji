f950ad47f93cb3b5e1c98e95d2399a6f9dbd1495
Yizack Rangel <yizackr@gmail.com>
2024-07-14 22:07:33 -0500

    feat: enable `Twemojify` caching for svg duplicates

--- src/runtime/components/Twemojify.vue
+++ src/runtime/components/Twemojify.vue
@@ -1,8 +1,9 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
-import { ref, watchEffect } from 'vue'
+import { ref, watch } from 'vue'
+import type { NuxtTwemojiRuntimeOptions } from '../../types'
 import { parse } from '../utils/twemojify'
-import { useState } from '#imports'
+import { useState, useAppConfig } from '#imports'
 
 const props = defineProps({
   text: {
@@ -18,31 +19,54 @@ const props = defineProps({
 const twemojify = useState(`twemojify:${props.png ? 'png' : 'svg'}`, () => ({}) as { [key: string]: string })
 const parsedText = ref(props.text)
 
+const replaceEmojis = (emoji: string, indices: number[]) => {
+  return parsedText.value.replace(props.text.slice(...indices), twemojify.value[emoji])
+}
+
 const loadTwemojify = async () => {
   const emojis = parse(props.text, { assetType: props.png ? 'png' : 'svg' })
-  for (const { url, indices, text } of emojis) {
-    if (twemojify.value[text]) {
-      parsedText.value = parsedText.value.replace(props.text.slice(...indices), twemojify.value[text])
+  for (const { url, indices, text: emoji } of emojis) {
+    if (twemojify.value[emoji]) {
+      parsedText.value = replaceEmojis(emoji, indices)
       continue
     }
 
     if (props.png) {
-      twemojify.value[text] = `<img src="${url}" class="twemojify" />`
+      twemojify.value[emoji] = `<img src="${url}" class="twemojify" />`
     }
     else {
+      if (import.meta.client) {
+        const { expiresIn } = useAppConfig().twemoji as NuxtTwemojiRuntimeOptions
+        const expiry = localStorage.getItem(`twemoji-expiry`)
+        if (!expiresIn || Date.now() > Number(expiry)) {
+          localStorage.removeItem(`twemojify-${emoji}`)
+          localStorage.setItem(`twemoji-expiry`, String(Date.now() + expiresIn * 1000)) // expires after 1 year
+        }
+
+        const cached = localStorage.getItem(`twemojify-${emoji}`)
+        if (cached) {
+          twemojify.value[emoji] = cached
+          parsedText.value = replaceEmojis(emoji, indices)
+          continue
+        }
+      }
       const svgFetch = await $fetch(url, { responseType: 'text' }).then(res => res as string).catch(() => '')
       const svgBody = svgFetch.replace(/<\/*svg[^>]*>/g, '')
       const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36" class="twemojify">${svgBody}</svg>`
-      twemojify.value[text] = svg
+      twemojify.value[emoji] = svg
+
+      if (import.meta.client) {
+        localStorage.setItem(`twemojify-${emoji}`, svg)
+      }
     }
-    parsedText.value = parsedText.value.replace(props.text.slice(...indices), twemojify.value[text])
+    parsedText.value = replaceEmojis(emoji, indices)
   }
 }
 
-watchEffect(async () => {
+watch(() => props, async () => {
   parsedText.value = props.text
   await loadTwemojify()
-})
+}, { deep: true })
 
 await loadTwemojify()
 </script>
