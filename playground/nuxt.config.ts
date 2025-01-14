ff7c7857ce4ca9b6818be993cd0eadbb0e51168e
Yizack Rangel <yizackr@gmail.com>
2025-01-13 19:43:42 -0500

    chore: update dependencies

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,11 +1,11 @@
 export default defineNuxtConfig({
   modules: ['nuxt-twemoji'],
-  twemoji: {
-
+  imports: {
+    autoImport: true,
   },
   devtools: { enabled: true },
   compatibilityDate: '2024-07-14',
-  imports: {
-    autoImport: true,
+  twemoji: {
+
   },
 })
